822c059598a8a89cdbfcfcf584d1d1e4df23d1eb
hanh2fpts <hanh2fpts.com.vn>
2023-04-20 17:45:31 +0700

    update code

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:go_router/go_router.dart';
+import 'package:meditation_app/routes.dart';
 import 'package:meditation_app/utils/color_constant.dart';
 
 class LoginPage extends StatelessWidget {
@@ -8,28 +10,44 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        SvgPicture.asset(
-          'assets/icons/logo_login.svg',
-          width: MediaQuery.of(context).size.width,
-        ),
-        const Text(
-          'We are what we do',
-          style: TextStyle(fontSize: 30, fontFamily: 'Roboto_Bold'),
-        ),
-        const Text(
-          'Thousand of people are usign silent moon \n for smalls meditation',
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              color: Color(0xFFA1A4B2), fontSize: 15, height: 2.0, fontFamily: 'Roboto_Light'),
-        ),
+      body: Stack(children: [
+        Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
+          SvgPicture.asset(
+            'assets/icons/logo_login.svg',
+            width: MediaQuery.of(context).size.width,
+          ),
+          const Text(
+            'We are what we do',
+            style: TextStyle(fontSize: 30, fontFamily: 'Roboto_Bold'),
+          ),
+          const Text(
+            'Thousand of people are usign silent moon \n for smalls meditation',
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                color: Color(0xFFA1A4B2), fontSize: 15, height: 2.0, fontFamily: 'Roboto_Light'),
+          ),
+        ]),
+        const Positioned.fill(bottom: 0, child: MyBottomWidget())
+      ]),
+    );
+  }
+}
+
+class MyBottomWidget extends StatelessWidget {
+  const MyBottomWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
         SizedBox(
           height: 50,
           width: MediaQuery.of(context).size.width - 40,
           child: ElevatedButton(
               style: ButtonStyle(
                   backgroundColor: MaterialStateProperty.all(colorContant.primaryColor)),
-              onPressed: () => print(''),
+              onPressed: () => GoRouter.of(context).goNamed(RouteName.signUp),
               child: const Text(
                 'SIGN UP',
                 style: TextStyle(color: Colors.white, fontSize: 18),
@@ -51,7 +69,7 @@ class LoginPage extends StatelessWidget {
                 ))
           ],
         )
-      ]),
+      ],
     );
   }
 }
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,24 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:meditation_app/presentation/login/login.dart';
+import 'package:meditation_app/presentation/login/sign_in.dart';
+import 'package:meditation_app/presentation/login/sign_up.dart';
 import 'package:meditation_app/presentation/topic/topic_page.dart';
 import 'package:meditation_app/presentation/welcome/welcome.dart';
 
 final router = GoRouter(
   routes: [
     GoRoute(
-        name: RouteName.welcomePage,
+        name: RouteName.logIn,
         path: '/',
         builder: (context, state) => const LoginPage(),
         routes: [
           GoRoute(
+            name: RouteName.welcomePage,
+            path: 'welcomePage',
+            pageBuilder: (context, state) {
+              return customTransitionPage(page: const WelcomePage(), state: state);
+            },
+          ),
+          GoRoute(
             name: RouteName.topicPage,
             path: 'topicPage',
+            pageBuilder: (context, state) {
+              return customTransitionPage(page: const TopicPage(), state: state);
+            },
+          ),
+          GoRoute(
+            name: RouteName.signUp,
+            path: 'signUp',
             pageBuilder: (context, state) {
               return CustomTransitionPage(
                 transitionDuration: const Duration(milliseconds: 500),
                 key: state.pageKey,
-                child: const TopicPage(),
+                child: const SignUpPage(),
                 transitionsBuilder: (context, animation, secondaryAnimation, child) {
                   return FadeTransition(
                     opacity: CurveTween(curve: Curves.bounceInOut).animate(animation),
@@ -28,6 +44,13 @@ final router = GoRouter(
               );
             },
           ),
+          GoRoute(
+            name: RouteName.signIn,
+            path: 'signIn',
+            pageBuilder: (context, state) {
+              return customTransitionPage(page: const SignInPage(), state: state);
+            },
+          ),
         ]),
   ],
 );
@@ -35,4 +58,21 @@ final router = GoRouter(
 class RouteName {
   static const String welcomePage = 'welcomePage';
   static const String topicPage = 'topicPage';
+  static const String signUp = 'signUp';
+  static const String signIn = 'signIn';
+  static const String logIn = 'logIn';
+}
+
+Page customTransitionPage({required Widget page, required GoRouterState state}) {
+  return CustomTransitionPage(
+    transitionDuration: const Duration(milliseconds: 500),
+    key: state.pageKey,
+    child: page,
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return FadeTransition(
+        opacity: CurveTween(curve: Curves.bounceInOut).animate(animation),
+        child: child,
+      );
+    },
+  );
 }
